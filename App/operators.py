ef0d7e65937b0ddd57c4e66dbd7d4f5d7315f709
totyka97 <tamas970906@gmail.com>
2024-01-14 16:25:02 +0100

    összeadás művelete elkészítve a számológéphez

---
+++ App/operators.py
@@ -0,0 +1,13 @@
+#Összeadás művelete
+def Addition(A:int, B:int):
+    try:
+        if (float(A)+float(B)) % (float(A)+float(B)).__round__(0) == 0:
+            return int(float(A)+float(B))
+        else:
+            return float(A)+float(B)
+    except ValueError:
+        return "Értékhiba"
+    except TypeError:
+        return "Helytelen típus"
+    except:
+        return "Err"
