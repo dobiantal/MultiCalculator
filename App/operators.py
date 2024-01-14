d8858266f7eb9004bca8bf8b1ca8257516891ac2
totyka97 <tamas970906@gmail.com>
2024-01-14 16:25:40 +0100

    Osztás elkészítése

--- App/operators.py
+++ App/operators.py
@@ -11,3 +11,14 @@ def Addition(A:int, B:int):
         return "Helytelen típus"
     except:
         return "Err"
+
+#Osztás művelete
+def div(OPERANDUS_A , OPERANDUS_B):
+    try:
+        return int(OPERANDUS_A) / int(OPERANDUS_B)
+    except ValueError:
+        return "Hibás érték!"
+    except SyntaxError:
+        return "Szintaktikai hiba!"
+    except ZeroDivisionError:
+        return "0-val való osztás 0-t ad!"
