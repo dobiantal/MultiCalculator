38cc0e392fbdb3a6625379f45fb9dae7f33f1613
totyka97 <tamas970906@gmail.com>
2024-01-14 11:04:22 +0100

    Label és button felhelyezése

--- App/mainView.py
+++ App/mainView.py
@@ -12,8 +12,19 @@ From_top_left: int = 500
 From_top: int = 200
 window.geometry(f'{Width}x{Height}+{From_top_left}+{From_top}')
 
+#Képernyőn látható szöveg
+kepernyoszoveg = "Dobi Antal és Tatár Tamás projekt munkája"
+udvozloLabel = tk.Label(window,text=kepernyoszoveg)
+udvozloLabel.place(relwidth=0.5, relheight=0.15, relx=0.25, rely=0.1)
 
+#számológép gomb
+szamologepBTN = tk.Button(window,text="Számológép")
+#szamologepBTN.place(x=200,y=200)
+szamologepBTN.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.3)
 
+#átváltó gomb
+atvaltoBTN = tk.Button(window,text="Átváltó")
+atvaltoBTN.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.5)
 
 
 
