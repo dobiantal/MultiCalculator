9a1475417789448201db0b857770733fd1ee3466
totyka97 <tamas970906@gmail.com>
2024-01-14 16:28:17 +0100

    Kivonás elkészítése

--- App/operators.py
+++ App/operators.py
@@ -1,4 +1,5 @@
 import math
+from decimal import Decimal, getcontext
 
 #Összeadás művelete
 def Addition(A:int, B:int):
@@ -58,3 +59,15 @@ def negyzetgyok(OPERANDUS_A):
         return "TERR"
     except:
         return "ERR"
+
+#kivonás művelete
+def subtract(OPERANDUS_A, OPERANDUS_B):
+    # Beállítja a kívánt pontosságot (8 tizedesjegy)
+    getcontext().prec = 10
+
+    try:
+        eredmeny = Decimal(OPERANDUS_A) - Decimal(OPERANDUS_B)
+        return round(eredmeny, 8)
+    except Exception as e:
+        print("Hiba történt a kivonás során:", e)
+        return None  # Jelzés hogy a művelet nem sikerült
