1c521f71f562515c50e3265091222863595c77b3
totyka97 <tamas970906@gmail.com>
2024-01-14 15:32:44 +0100

    igazítgatások

--- App/AtvaltoView.py
+++ App/AtvaltoView.py
@@ -1,138 +1,136 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 
+def AtvaltoMegjelen():
+    """*A főablak implementálása."""
+    window = tk.Tk()
+    window.title("Atváltó")
+    #combo = ttk.Combobox()
+    Width: int = 600
+    Height: int = 600
+    From_top_left: int = 500
+    From_top: int = 200
+    window.geometry(f'{Width}x{Height}+{From_top_left}+{From_top}')
 
-"""*A főablak implementálása."""
-window = tk.Tk()
-window.title("Atváltó")
-combo = ttk.Combobox()
-Width: int = 600
-Height: int = 600
-From_top_left: int = 500
-From_top: int = 200
-window.geometry(f'{Width}x{Height}+{From_top_left}+{From_top}')
+    ertek=0
 
-ertek=0
-
-merteklista : list = []
-tipusLista =["Távolság", "Tömeg", "Térfogat"]
-comboTipus = ttk.Combobox(
-    state="readonly",
-    values = tipusLista,
-    width=10
-)
+    merteklista = []
+    tipusLista =["Távolság", "Tömeg", "Térfogat"]
+    comboTipus = ttk.Combobox(window,
+        state="readonly",
+        values = tipusLista,
+        width=10
+    )
 
 
-#ez a metódus fogja beállítani a kijelölt opciónak megfelelően a választható értékeket
-def kivalasztottLista(event):
-    global merteklista
-    if comboTipus.get() == "Távolság":
-        merteklista.clear()
-        merteklista = ["miles","km","m","dm","cm","mm"]
-        comboMirol['values'] = ["km", "m", "dm", "cm", "mm"]
-        comboMire['values'] = ["km", "m", "dm", "cm", "mm"]
+    #ez a metódus fogja beállítani a kijelölt opciónak megfelelően a választható értékeket
+    def kivalasztottLista(event):
+        global merteklista
+        if comboTipus.get() == "Távolság":
 
-    if comboTipus.get() == "Tömeg":
-        merteklista.clear()
-        merteklista = ["t","kg","dkg","g"]
-        comboMirol['values'] = ["t","kg","dkg","g"]
-        comboMire['values'] = ["t","kg","dkg","g"]
+            comboMirol['values'] = ["km", "m", "dm", "cm", "mm"]
+            comboMire['values'] = ["km","m" , "dm", "cm", "mm"]
 
-    if comboTipus.get() == "Térfogat":
-        merteklista.clear()
-        merteklista = ["m3","hl","l","dl","cl","ml"]
-        comboMirol['values'] = ["m3","hl","l","dl","cl","ml"]
-        comboMire['values'] = ["m3","hl","l","dl","cl","ml"]
+        if comboTipus.get() == "Tömeg":
+
+            comboMirol['values'] = ["t","kg","dkg","g"]
+            comboMire['values'] = ["t","kg","dkg","g"]
+
+        if comboTipus.get() == "Térfogat":
+
+            comboMirol['values'] = ["m3","hl","l","dl","cl","ml"]
+            comboMire['values'] = ["m3","hl","l","dl","cl","ml"]
 
 
 
-comboTipus.bind("<<ComboboxSelected>>", kivalasztottLista)
+    comboTipus.bind("<<ComboboxSelected>>", kivalasztottLista)
 
-#A miről való textbox kitöltése
-textMirol= tk.Text(window,
-                   height = 2,
-                   width = 10
-                   )
-
-
-#első legördülő lista a miről való kiválasztása
-comboMirol = ttk.Combobox(
-    state="readonly",
-    values = merteklista,
-    width=5
-)
-
-# Konvertáló gomb
-btn_convert = tk.Button(
-    master=window,
-    width = 10,
-    text="\N{RIGHTWARDS BLACK ARROW}"
-)
-
-#viszatérő érték
-labelMire = tk.Label(window,
-                     text=ertek,
-                     width=5)
-
-#első legördülő lista a mire való kiválasztása
-comboMire = ttk.Combobox(
-    state="readonly",
-    values = merteklista,
-    width=5
-
-)
-Y_tipussor = 50
-Y_atvaltas = 260
-
-#Gombok és legördülő listák pozícionálása
-comboTipus.place(x=50, y=Y_tipussor)
-comboMirol.place(x=160, y=Y_atvaltas)
-btn_convert.place(x=250, y=Y_atvaltas)
-comboMire.place(x=450, y=Y_atvaltas)
-textMirol.place(x=80,y=Y_atvaltas)
-labelMire.place(x= 390,y=Y_atvaltas)
+    #A miről való textbox kitöltése
+    textMirol= tk.Text(window,
+                       height = 2,
+                       width = 10
+                       )
 
 
-#kell egy textbox érték
-ertek = textMirol.get("1.0", "end-1c")
+    #első legördülő lista a miről való kiválasztása
+    comboMirol = ttk.Combobox(window,
+        state="readonly",
+        values = merteklista,
+        width=5
+    )
 
-#Összefogó metódus at átváltó gombra
-def atvalt(event):
-    ertekadas()
-    kiszamoltErtek()
+    # Konvertáló gomb
+    btn_convert = tk.Button(
+        master=window,
+        width = 10,
+        text="\N{RIGHTWARDS BLACK ARROW}"
+    )
 
-    #Ha helyes az érték
-    if ertekHelyes() :
-        print(ertek)
+    #viszatérő érték
+    labelMire = tk.Label(window,
+                         text=ertek,
+                         width=5)
+
+    #első legördülő lista a mire való kiválasztása
+    comboMire = ttk.Combobox(window,
+        state="readonly",
+        values = merteklista,
+        width=5
+
+    )
+    Y_tipussor = 50
+    Y_atvaltas = 260
+
+    #Gombok és legördülő listák pozícionálása
+    comboTipus.place(x=50, y=Y_tipussor)
+    comboMirol.place(x=160, y=Y_atvaltas)
+    btn_convert.place(x=250, y=Y_atvaltas)
+    comboMire.place(x=450, y=Y_atvaltas)
+    textMirol.place(x=80,y=Y_atvaltas)
+    labelMire.place(x= 390,y=Y_atvaltas)
 
 
-def ertekHelyes():
-    global ertek
-    try:
-        ertek = int(ertek)
-        return True
-    except TypeError:
-        print("típus hiba")
-    except :
-        print("hiba")
-
-
-
-def ertekadas():
-    global ertek
+    #kell egy textbox érték
     ertek = textMirol.get("1.0", "end-1c")
 
-#A kiszámolt mezőbe beírja az értéket
-def kiszamoltErtek():
-    global ertek
-    ertek = int(ertek)
-    ertek = ertek * 1000
+    #Összefogó metódus at átváltó gombra
+    def atvalt(event):
+        ertekadas()
+        kiszamoltErtek()
 
-    labelMire['text']=ertek
-
-
-btn_convert.bind("<Button-1>",atvalt)
+        #Ha helyes az érték
+        if ertekHelyes() :
+            print(ertek)
 
 
+    def ertekHelyes():
+        global ertek
+        try:
+            ertek = int(ertek)
+            return True
+        except TypeError:
+            print("típus hiba")
+        except :
+            print("hiba")
 
-window.mainloop()
+
+
+    def ertekadas():
+        global ertek
+        ertek = textMirol.get("1.0", "end-1c")
+
+    #A kiszámolt mezőbe beírja az értéket
+    def kiszamoltErtek():
+        global ertek
+        ertek = int(ertek)
+        ertek = ertek * 1000
+
+        labelMire['text']=ertek
+
+
+    btn_convert.bind("<Button-1>",atvalt)
+
+
+
+    window.mainloop()
+
--- App/mainView.py
+++ App/mainView.py
@@ -1,39 +1,39 @@
 import tkinter as tk
-from tkinter import messagebox, ttk
 
+import AtvaltoView
 
 """*A főablak implementálása."""
-window = tk.Tk()
-window.title("MultiCalculator")
-combo = ttk.Combobox()
+windowMain = tk.Tk()
+windowMain.title("MultiCalculator")
+#combo = ttk.Combobox()
 Width: int = 600
 Height: int = 600
 From_top_left: int = 500
 From_top: int = 200
-window.geometry(f'{Width}x{Height}+{From_top_left}+{From_top}')
+windowMain.geometry(f'{Width}x{Height}+{From_top_left}+{From_top}')
 
 #Képernyőn látható szöveg
 kepernyoszoveg = "Dobi Antal és Tatár Tamás projekt munkája"
-udvozloLabel = tk.Label(window,text=kepernyoszoveg)
+udvozloLabel = tk.Label(windowMain, text=kepernyoszoveg)
 udvozloLabel.place(relwidth=0.5, relheight=0.15, relx=0.25, rely=0.1)
 
 #számológép gomb
-szamologepBTN = tk.Button(window,text="Számológép")
+szamologepBTN = tk.Button(windowMain, text="Számológép")
 #szamologepBTN.place(x=200,y=200)
 szamologepBTN.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.3)
 
 #átváltó gomb
-atvaltoBTN = tk.Button(window,text="Átváltó")
+atvaltoBTN = tk.Button(windowMain, text="Átváltó" )
 atvaltoBTN.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.5)
 
 #számológép button metódus
 def szamologepMegnyit(event):
-    print("szamologépmegnyitás")
+   AtvaltoView.AtvaltoMegjelen()
 
 
 #átváltó button metódis
 def atvaltoMegnyit(event):
-    pass
+    AtvaltoView.AtvaltoMegjelen()
 
 #gombok esemény kezelése
 szamologepBTN.bind('<Button-1>',szamologepMegnyit)
@@ -47,4 +47,4 @@ atvaltoBTN.bind('<Button-1>',atvaltoMegnyit)
 
 
 
-window.mainloop()
+windowMain.mainloop()
