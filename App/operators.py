ef045597fd7e3fdee6e8808abb4c1a3a86bab301
totyka97 <tamas970906@gmail.com>
2024-01-14 16:26:33 +0100

    Szorzás elkészítése

--- App/operators.py
+++ App/operators.py
@@ -22,3 +22,16 @@ def div(OPERANDUS_A , OPERANDUS_B):
         return "Szintaktikai hiba!"
     except ZeroDivisionError:
         return "0-val való osztás 0-t ad!"
+
+#Szorzás művelete
+def multiplicate(OPERANDUS_A, OPERANDUS_B):
+    if not (str(OPERANDUS_A).isnumeric() and str(OPERANDUS_B).isnumeric()):
+        raise ValueError("Az OPERANDUS_A és OPERANDUS_B értékei csak számok lehetnek.")
+
+    OPERANDUS_A = float(OPERANDUS_A)
+    OPERANDUS_B = float(OPERANDUS_B)
+
+    if OPERANDUS_A == 0 or OPERANDUS_B == 0:
+        raise ValueError("Az OPERANDUS_A vagy OPERANDUS_B értéke nem lehet 0.")
+
+    return OPERANDUS_A * OPERANDUS_B
