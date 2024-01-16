0effccd1a88f67ac31af64089d33e9e5be7cc4db
totyka97 <tamas970906@gmail.com>
2024-01-16 20:27:53 +0100

    osszeadás unit test implementálása

--- App/operatorsUnitTest.py
+++ App/operatorsUnitTest.py
@@ -3,7 +3,7 @@ import unittest
 import App.operators as Operator
 
 
-class TestNegyzetgyok(unittest.TestCase):
+class UnitTest(unittest.TestCase):
 
     def test_negyzetgyok(self):
         # Tesztesetek:
@@ -13,3 +13,9 @@ class TestNegyzetgyok(unittest.TestCase):
         self.assertAlmostEqual(Operator.negyzetgyok(-12), 0)  # -12 négyzetgyöke 0
         self.assertAlmostEqual(Operator.negyzetgyok(14), 3.7416573867739413)  # nem egészre visszatérő érték ellenörzése
         self.assertAlmostEqual(Operator.negyzetgyok(4.5), 2.1213203435596424)  # nem egészre visszatérő érték ellenörzése
+
+    def test_osszeadas(self):
+        self.assertAlmostEqual(Operator.Addition(1,1),2)
+        self.assertAlmostEqual(Operator.Addition(13,45),58)
+        self.assertAlmostEqual(Operator.Addition(100,6543),6643)
+        self.assertAlmostEqual(Operator.Addition(19765,20),19785)
